4aa7deb1860b02c00e73de3e44c11fa7c1f30c16
jonnahfreire <jonnas.freire17@gmail.com>
2021-03-14 00:58:38 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,443 @@
+import React, { useState, useEffect } from 'react';
+
+import {
+  AntDesign,
+} from '@expo/vector-icons';
+
+
+import { 
+  TouchableOpacity,
+  StyleSheet, 
+  Text, 
+  View, 
+  KeyboardAvoidingView,
+  StatusBar,
+  ScrollView,
+  RefreshControl,
+} from 'react-native';
+
+import Card from './components/Card';
+import CreateNote from './components/CreateNote';
+import EditNote from './components/EditNote';
+import ColorPic  from './components/ColorPicker';
+
+import { storeNote, getNotes } from './Storage';
+const editData = {};
+
+let cardId = 0;
+
+function App() {
+  const [ showNewNoteContainer, setShowNewNoteContainer ] = useState(false)
+  const [ showEditNoteContainer, setShowEditNoteContainer ] = useState(false)
+  const [ showPicker, setShowPicker ] = useState(false);
+  const [ changeCardColor, setCardColor ] = useState();
+
+  const [ notes, setNotes ] = useState([]);
+  const [ refreshing, setRefreshing ] = useState(false);
+  const [ deleteCard, setDeleteCard ] = useState();
+
+  const [ alert, setAlert ] = useState({
+    "alert": false,
+    "msg":""
+  });
+
+  const confirmExclusion = (confirmed=false, id="")=> {
+    setDeleteCard(id)
+
+    !confirmed &&
+      setAlert({
+          "alert":true,
+          "msg":"Deseja realmente excluir?"
+      })
+  }
+
+  const onRefresh = (time=0) => {
+    setRefreshing(true);
+    const fetchData = async ()=> {
+      const data = await getNotes();
+      if(data === undefined) {
+        setNotes([])
+        return;
+      }
+      setNotes(data)
+    }
+
+    fetchData();
+    
+    setTimeout(() => {
+      setRefreshing(false)
+    }, time)
+  }
+
+  const handleEdit = (editTitle,
+    editText, bgColor, id) => {
+    editData.title = editTitle;
+    editData.note = editText;
+    editData.id = id;
+    editData.cardColor = bgColor;
+    setShowEditNoteContainer(true)
+  }
+  
+  const handleEditCardColor = id => 
+  { 
+    cardId = id
+    setCardIdColor(id)
+    setShowPicker(true)
+  }
+  
+  const handlePressCloseBtn = () => {
+    setShowNewNoteContainer(false)
+  }
+  const handlePressEditCloseBtn = () => {
+    setShowEditNoteContainer(false)
+  }
+  
+  const setNewNote = (
+    title, text, bgcolor,
+    editting=false,
+    editId=0) => {
+      
+      if(editting){
+        notes.splice(editId, 1)
+      }
+      
+      notes.unshift({
+        "bgcolor": bgcolor,
+        "time": time(),
+        "title":title,
+        "text":text
+      })
+      
+      storeNote(notes)
+    }
+    
+  const deleteNote = (id) => {
+    notes.splice(id, 1)
+    storeNote(notes)
+    setNotes(notes)
+    onRefresh(5)
+  }
+
+  useEffect(() => {
+    onRefresh(1000)
+  },[])
+  
+  return (
+    <KeyboardAvoidingView style={styles.background}>
+      <View style={styles.container}>
+        <StatusBar barStyle = "light-content"/>
+        
+        <View style={styles.titleView}>
+          <Text style={styles.appTitle}>Notes</Text>
+        </View>
+
+        <ScrollView 
+          style={styles.cardsContainer}
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl
+            refreshing={refreshing}
+            onRefresh={()=>onRefresh(2000)}
+            />
+          }
+          > 
+          {notes!== undefined && notes?.length>0 && notes.map((note, key) => 
+              <Card
+                key={key}
+                id={key}
+                title={note.title}
+                note={note.text}
+                time={note.time}
+                cardColor={note.bgcolor}
+                delete={deleteNote}
+                setNote={setNewNote}
+                edit={handleEdit}
+                editCardColor={handleEditCardColor}
+                showPicker={setShowPicker}
+                confirmExclusion={confirmExclusion}
+              />
+          )}
+        </ScrollView>
+          {notes.length==0 &&
+            <Text style={styles.msg}>
+              Clique no botão (+) para criar uma nota
+            </Text>
+          }
+          {showNewNoteContainer &&
+            <CreateNote
+              show={handlePressCloseBtn}
+              setNote={setNewNote}
+            />
+          }
+          {/* Unused Component */}
+          {showPicker &&
+            <ColorPic show={setShowPicker} setColor={setCardColor}/>
+          }
+          {alert.alert &&
+            <View style={styles.alertContainer}>
+              <View style={styles.alert}>
+                <Text style={styles.alertText}>
+                  {alert.msg}
+                </Text>
+
+                {alert.msg &&
+                  <View style={styles.alertBtnYN}>
+                    <TouchableOpacity style={styles.alertBtnY}>
+                      <Text
+                        style={styles.alertBtnTextY}
+                        onPress={()=>{
+                          deleteNote(deleteCard)
+                          setAlert(false)
+                      }}>
+                        Sim
+                      </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.alertBtnN}>
+                      <Text
+                        style={styles.alertBtnTextN}
+                        onPress={()=>{setAlert(false)}}
+                      >
+                        Não
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                }
+              </View>
+            </View>
+          }
+          {showEditNoteContainer && editData &&
+            <EditNote
+              show={handlePressEditCloseBtn}
+              setNote={setNewNote}
+              data={editData}
+            />
+          }
+        <TouchableOpacity
+          style={styles.btnCreateNewNote}
+          onPress={()=>setShowNewNoteContainer(true)}
+        >
+          <AntDesign
+            name="plus"
+            size={30}
+            color="white"
+          />
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    backgroundColor: '#333333',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    backgroundColor: '#060359',
+    flex:1,
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    width: '100%',
+  },
+  msg: {
+    position: 'absolute',
+    width: '100%',
+    height: 30,
+    fontSize: 25,
+    top: '50%',
+    textAlign: 'center',
+    color: '#FFF',
+  },
+  titleView:{
+    width: '100%',
+    height: 70,
+    backgroundColor: '#6d0496',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: {width: -1, height: 1},
+  },
+  appTitle: {
+    fontSize: 40,
+    color: '#FFF'
+  },
+  cardsContainer: {
+    width: '100%',
+    maxHeight: '77%',
+    flex: 1,
+    marginTop: 15,
+  },
+  btnCreateNewNote: {
+    width: 45,
+    height: 45,
+    position: 'absolute',
+    bottom: 15,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#8603ba',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25,
+  }, alertContainer: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    top: 0.2,
+    backgroundColor: 'rgba(160, 160, 160, .2)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 999
+  },
+  alert: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    position: 'relative',
+    top: '-10%',
+    width: '90%',
+    height: '20%',
+    backgroundColor: '#2d2d2d',
+    color: '#FFF',
+    textAlign: 'center',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+  alertText: {
+    width: '100%',
+    height: 65,
+    fontSize: 25,
+    color: '#FFF',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+  alertBtn: {
+    backgroundColor: 'rgba(145, 145, 145, .3)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 45,
+    height: 45,
+    maxHeight: 45,
+    marginTop: 10,
+    marginRight: -245,
+    textAlign: 'center',
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+  alertBtnY: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 45,
+    height: 45,
+    maxHeight: 45,
+    marginTop: 10,
+  },
+  alertBtnN: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 45,
+    height: 45,
+    maxHeight: 45,
+    marginTop: 10,
+  },
+  alertBtnYN: {
+    width: '100%',
+    display:'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  alertBtnText: {
+    color: '#FFF',
+    width: '100%',
+    fontSize: 25,
+    textAlign: 'center',
+    alignSelf: 'flex-start',
+  },
+  alertBtnTextY: {
+    backgroundColor: '#1c8dd8',
+    color: '#FFF',
+    width: '100%',
+    fontSize: 25,
+    width: 52,
+    height: 45,
+    paddingTop: 7,
+    textAlign: 'center',
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+  alertBtnTextN: {
+    backgroundColor: 'rgba(145, 5, 5, 1)',
+    color: '#FFF',
+    width: '100%',
+    fontSize: 25,
+    width: 52,
+    height: 45,
+    paddingTop: 7,
+    textAlign: 'center',
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+
+})
+
+const time = () => {
+  const months = ['Jan','Fev','Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  hours = hours.toString().length === 1 ? `0${hours}`: hours;
+  minutes = minutes.toString().length == 1 ? `0${minutes}`: minutes;
+  date = `${day} ${months[month]} ${hours}:${minutes}`;
+  return date;
+}
+
+export default App;
+
+// '#288A0B'03bc22
+// import React from 'react';
+
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// import Home from './components/Home';
+// import Sobre from './components/Sobre';
+// import Perfil from './components/Perfil';
+// import { View } from 'react-native';
+
+// const Stack = createStackNavigator();
+// const Tab = createBottomTabNavigator();
+
+// function Tabs() {
+//   return (
+//     <Tab.Navigator>
+//       <Tab.Screen name="Feed" component={Home} />
+//       <Tab.Screen name="Sobre" component={Sobre} />
+//       <Tab.Screen name="Perfil" component={Perfil} />      
+//     </Tab.Navigator>
+//   )  
+// }
---
+++ components/CreateNote/index.js
@@ -0,0 +1,387 @@
+import React, { useState } from 'react';
+
+import { 
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+  Keyboard
+} from 'react-native';
+
+import {
+  AntDesign,
+  FontAwesome,
+  MaterialCommunityIcons
+} from '@expo/vector-icons'
+
+import ColorPic  from '../ColorPicker';
+
+export default function CreateNote(props) {
+    const [title, setTitle] = useState("");
+    const [text, setText]   = useState("");
+    const [cardColor, setCardColor] = useState("#11bf5f");
+    const [showPicker, setShowPicker] = useState(false);
+
+    const [alert, setAlert] = useState({
+      "alert": false, 
+      "msg":""
+    });
+
+    const saveNote = () => {
+      Keyboard.dismiss()
+      if(!text && !title || title && !text) {
+        setAlert({
+          "alert":true,
+          "msg":"Impossível salvar notas vazias!"
+        })
+
+      }else if(text && !title) {
+        setAlert({
+          "alert":true,
+          "msg":"Por favor insira um título"
+        })
+
+      }else if(title && text){
+        props.setNote?.(
+          title,
+          text,
+          cardColor
+        )
+        props.show?.()
+      }
+    }
+
+    const verifyEdited = () => {
+      Keyboard.dismiss()
+      
+      if(title.length>0 
+        || text.length>0 
+        || cardColor!=="#11bf5f"
+      ){
+        setAlert({
+          "alert":true,
+          "msg":"Deseja salvar as alterações?"
+        })
+
+      }else{
+        props.show?.()
+      }
+    }
+
+    return(
+      <View style={styles.newNoteContainerModal}>
+        {alert.alert &&
+          <View style={styles.alertContainer}>
+            <View style={styles.alert}>
+              <Text style={styles.alertText}>
+                {alert.msg}
+              </Text>
+              {!alert.msg.includes("alterações")  && 
+                <TouchableOpacity style={styles.alertBtn}>
+                  <Text
+                    style={styles.alertBtnText}
+                    onPress={()=>setAlert(false)}
+                  >
+                    OK
+                  </Text>
+                </TouchableOpacity>
+              }
+              {alert.msg.includes("alterações") &&
+                <View style={styles.alertBtnYN}>
+                  <TouchableOpacity style={styles.alertBtnY}>
+                    <Text
+                      style={styles.alertBtnTextY}
+                      onPress={()=>{saveNote()}}
+                    >
+                      Sim
+                    </Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity style={styles.alertBtnN}>
+                    <Text
+                      style={styles.alertBtnTextN}
+                      onPress={()=>{setAlert(false); props.show?.()}}
+                    >
+                      Não
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              }
+            </View>
+          </View>
+        }
+        <View style={{
+          position: 'absolute',
+          backgroundColor: cardColor !== '#11bf5f'? cardColor: "#515151",
+          top: '2%',
+          width: '95%',
+          height: '70%',
+          borderRadius: 15
+        }}>
+          <View style={styles.headerContainer}>
+            <TextInput
+              style={styles.newNoteTitle}
+              autoCorrect={false}
+              placeholder="Insira um título"
+              placeholderTextColor="#FFF"
+              value={title}
+              onChangeText={(value)=>setTitle(value)}
+            />
+            <TouchableOpacity
+              style={styles.btnCloseNewNote}
+              onPress={()=>{verifyEdited()}}
+            >
+              <AntDesign
+                name="close"
+                size={30}
+                color="white"
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.newNoteTextContainer}>
+            <TextInput
+              style={styles.newNoteText}
+              autoCorrect={false}
+              multiline={true}
+              numberOfLines = {30}
+              value={text}
+              placeholder="Clique para começar a digitar"
+              placeholderTextColor="#FFF"
+              onChangeText={(value)=>setText(value)}
+            />
+          </View>
+          
+          <View style={styles.newNoteBtnContainer}>
+            <TouchableOpacity
+              style={styles.newNoteBtnColorPalette}
+              onPress={()=> {
+                Keyboard.dismiss()
+                setShowPicker(true) 
+              }}
+            >
+              <MaterialCommunityIcons
+                name="palette-outline"
+                size={30}
+                color="white"
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.newNoteBtnSave}
+              onPress={()=>{
+                Keyboard.dismiss()
+                saveNote()
+              }}
+            >
+              <FontAwesome
+                name="save"
+                size={30}
+                color="white"
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        {showPicker &&
+          <ColorPic 
+            show={setShowPicker} 
+            setColor={setCardColor}
+          />
+        }
+      </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  newNoteContainerModal: {
+    backgroundColor: 'rgba(0, 0, 0,.7)',
+    position: 'absolute',
+    top: 67,
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    height: '100%',
+    width: '100%',
+    zIndex: 999
+  },
+  alertContainer: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    top: 0.2,
+    backgroundColor: 'rgba(160, 160, 160, .2)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 999
+  },
+  alert: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    position: 'relative',
+    top: '-10%',
+    width: '90%',
+    height: '20%',
+    backgroundColor: '#2d2d2d',
+    color: '#FFF',
+    textAlign: 'center',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+  alertText: {
+    width: '100%',
+    height: 65,
+    fontSize: 25,
+    color: '#FFF',
+    textAlign: 'center',
+    textAlignVertical: 'center'
+  },
+  alertBtn: {
+    backgroundColor: 'rgba(145, 145, 145, .3)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 45,
+    height: 45,
+    maxHeight: 45,
+    marginTop: 10,
+    marginRight: -245,
+    textAlign: 'center',
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+  alertBtnY: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 45,
+    height: 45,
+    maxHeight: 45,
+    marginTop: 10,
+  },
+  alertBtnN: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 45,
+    height: 45,
+    maxHeight: 45,
+    marginTop: 10,
+  },
+  alertBtnYN: {
+    width: '100%',
+    display:'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  alertBtnText: {
+    color: '#FFF',
+    width: '100%',
+    fontSize: 25,
+    textAlign: 'center',
+    alignSelf: 'flex-end'
+  },
+  alertBtnTextY: {
+    backgroundColor: '#1c8dd8',
+    color: '#FFF',
+    width: '100%',
+    fontSize: 25,
+    width: 52,
+    height: 45,
+    paddingTop: 7,
+    textAlign: 'center',
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+  alertBtnTextN: {
+    backgroundColor: 'rgba(145, 5, 5, .9)',
+    color: '#FFF',
+    width: '100%',
+    fontSize: 25,
+    width: 52,
+    height: 45,
+    paddingTop: 7,
+    textAlign: 'center',
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+  headerContainer: {
+    backgroundColor: '#606060',
+    width: '100%',
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnCloseNewNote: {
+    width: 39,
+    height: '100%',
+    paddingTop: 7,
+    borderTopRightRadius: 15,
+  },
+  newNoteTitle: {
+    position: 'relative',
+    fontSize: 25,
+    width: '90%',
+    height: 45,
+    borderTopLeftRadius: 15,
+    paddingHorizontal: 5,
+    color: '#FFF',
+    textAlign: 'center',
+    
+  },
+  newNoteTextContainer: {
+    flex: 3.3
+    },
+    newNoteText: {
+    flex: 3,
+    fontSize: 25,
+    color: '#FFF',
+    letterSpacing: 2,
+    paddingHorizontal: 10,
+    textAlign: 'left'
+  },
+  newNoteBtnContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flex: 1
+  },
+  newNoteBtnSave: {
+    width: 75,
+    display: 'flex',
+    alignItems: 'center',
+    padding: 5,
+    textAlign: 'center',
+    backgroundColor: '#2dad06',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+  newNoteBtnColorPalette: {
+    width: 75,
+    display: 'flex',
+    alignItems: 'center',
+    padding: 5,
+    textAlign: 'center',
+    backgroundColor: '#a746f7',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+  }
+})
